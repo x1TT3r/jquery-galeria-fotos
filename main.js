18869ec25004c0b1f45ce1c855589e3111c170ca
x1TT3r <113714755+x1TT3r@users.noreply.github.com>
2022-10-03 20:28:04 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit',function(e) {
+        e.preventDefault();
+        const enderecoDaNovaImagem =$('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display:none"></li>');
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class ="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em Tamanho real">
+                    Ver Imagem em Tamanho Real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn();
+        $('#endereco-imagem-nova').val('');
+    })
+})
